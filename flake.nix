965442dd137d096cfee79be61fbc387e4451053b
Charles Eckman <charles@cceckman.com>
2022-12-12 15:37:56 +0000

    Port to Nix; add HTTP proxying

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A Tailscale reverse-proxy";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          tsproxy = pkgs.buildGoModule {
+            pname = "tsproxy";
+            inherit version;
+            src = ./.;
+            runVend = true;
+            # proxyVend = true;
+            vendorSha256 = "sha256-HSYkoEXdOPKzZ2P+YQqg8+cxSPUcQnPUQCguMbrVBjw=";
+          };
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.tsproxy);
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
+    };
+}
--- main.go
+++ main.go
@@ -1,4 +1,12 @@
-// `tsproxy` proxies a TLS connection in Tailscale to a local TCP or Unix socket.
+// `tsproxy` proxies an HTTP session in Tailscale to a local socket,
+// adding authentication headers along the way.
+//
+// This is a combination of two Tailscale programs:
+// - proxy-to-grafana, which acts as an reverse proxy that adds webauth headers
+// - nginx-auth, which acts on side-band to nginx to report additional headers
+//
+// This is a reverse proxy for a single servce, since I don't have an nginx
+// setup.
 package main
 
 import (
@@ -6,12 +14,16 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"io"
+	"io/fs"
 	"log"
 	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"tailscale.com/client/tailscale"
@@ -19,9 +31,13 @@ import (
 )
 
 var (
-	to   = flag.String("to", "", "(Local) address to proxy connections to")
-	from = flag.String("from", "aproxy", "Tailscale node name to use")
-	help = flag.Bool("help", false, "Display a usage message.")
+	to          = flag.String("to", "", "(Local) address to proxy connections to")
+	from        = flag.String("from", "", "Tailscale node name to register this proxy as")
+	help        = flag.Bool("help", false, "Display a usage message.")
+	emailHeader = flag.String("email-header", "X-Webauth-Email", "Header indicating the email address associated with the authenticated user")
+	netHeader   = flag.String("net-header", "X-Webauth-Network", "Header indicating the Tailscale network associated with the authenticated user")
+	userHeader  = flag.String("user-header", "X-Webauth-User", "Header indicating the user name (prefix of email address) associated with the authenticated user")
+	authKeyPath = flag.String("authkey-path", "", "If present, path of a file contianing a Tailscale auth key. Can be used in place of TS_AUTHKEY.")
 )
 
 func main() {
@@ -34,17 +50,68 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
+	if *to == "" || *from == "" {
+		fmt.Fprintf(os.Stderr, "Missing -to and/or -from")
+		flag.Usage()
+		os.Exit(1)
+	}
 
-	net, addr := getAddr(*to)
+	s := getServer()
+	c, err := s.LocalClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := getProxy(c)
+	l := getListener(s, c)
 
-	l := getListener(*from)
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
+	log.Printf("tsproxy running at %v, proxying to %v", l.Addr(), *to)
+	log.Fatal(http.Serve(l, p))
+}
+
+func getListener(s *tsnet.Server, c *tailscale.LocalClient) net.Listener {
+	ln, err := s.Listen("tcp", ":443")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tls.NewListener(ln, &tls.Config{
+		GetCertificate: c.GetCertificate,
+	})
+}
+
+func getProxy(client *tailscale.LocalClient) *httputil.ReverseProxy {
+	url, err := url.Parse(fmt.Sprintf("http://%s", *to))
+	if err != nil {
+		log.Fatal("invalid target address: ", err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	d := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		d(req)
+		authenticateRequest(req, client)
+	}
+	return proxy
+}
+
+func authenticateRequest(req *http.Request, client *tailscale.LocalClient) {
+	// Check auth on the inbound request.
+	ctx, cancel := context.WithTimeout(req.Context(), 5*time.Second)
+	defer cancel()
+	who, err := client.WhoIs(ctx, req.RemoteAddr)
+	if err != nil {
+		log.Default().Printf("error getting Tailscale whois info: %s", err)
+		return
+	}
+
+	req.Header.Add(*emailHeader, who.UserProfile.LoginName)
+	req.Header.Add(*userHeader, strings.Split(who.UserProfile.LoginName, "@")[0])
+	// nginx-auth proxy notes that on shared nodes, the tailnet won't be known.
+	if !who.Node.Hostinfo.ShareeNode() {
+		_, tailnet, ok := strings.Cut(who.Node.Name, who.Node.ComputedName+".")
+		if !ok {
+			log.Printf("can't extract tailnet name from hostname %q", who.Node.Name)
 		}
-		log.Printf("got connection from %v", conn.RemoteAddr())
-		go handleConnection(net, addr, conn)
+		tailnet = strings.TrimSuffix(tailnet, ".beta.tailscale.net")
+		req.Header.Add(*netHeader, tailnet)
 	}
 }
 
@@ -57,55 +124,30 @@ func getAddr(to string) (net, addr string) {
 	return "tcp", to
 }
 
-func getListener(name string) net.Listener {
+func getServer() *tsnet.Server {
 	// Interpolate "name" into the address, so two `tsproxy`s for the same user don't clobber each other.
 	userDir, err := os.UserConfigDir()
 	if err != nil {
 		log.Fatal(err)
 	}
-	dir := path.Join(userDir, fmt.Sprintf("tsproxy-%s", name))
+	dir := path.Join(userDir, fmt.Sprintf("tsproxy-%s", *from))
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		os.MkdirAll(dir, 0700)
 	}
-	s := tsnet.Server{
+
+	authKey, err := fs.ReadFile(os.DirFS(""), *authKeyPath)
+	if *authKeyPath != "" && err != nil {
+		log.Printf("could not read TS auth key from %s: %v; continuing", *authKeyPath, err)
+	}
+
+	s := &tsnet.Server{
 		Dir:      dir,
-		Hostname: name,
-	}
-	ln, err := s.Listen("tcp", ":443")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return tls.NewListener(ln, &tls.Config{
-		GetCertificate: tailscale.GetCertificate,
-	})
-}
-
-func handleConnection(localNet string, localAddr string, c net.Conn) {
-	defer c.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-
-	remoteAddr := c.RemoteAddr().String()
-
-	if who, err := tailscale.WhoIs(ctx, remoteAddr); err != nil {
-		log.Printf("error getting Tailscale whois info: %s", err)
-		return
-	} else {
-		log.Printf("established connection at %s to %s on %s", remoteAddr, who.UserProfile.LoginName, who.Node.Name)
+		Hostname: *from,
+		AuthKey:  string(authKey),
 	}
 
-	// Establish a new local connection.
-	localConn, err := net.Dial(localNet, localAddr)
-	if err != nil {
-		log.Printf("could not establish local connection: %s", err)
-		return
+	if err := s.Start(); err != nil {
+		log.Fatal("error starting Tailscale server: ", err)
 	}
-	defer localConn.Close()
-
-	// Copy local-to-remote in a background thread:
-	go io.Copy(localConn, c)
-
-	// In this thread, copy remote-to-local.
-	io.Copy(c, localConn)
-	// When we exit this scope, both connections are closed.
+	return s
 }
